c47d60d32a163e4d3fbd81052e8bdbd53a1d0e6a
PawnxPawn <pawnxpawnofficial+github@gmail.com>
2024-08-07 22:11:49 -0600

    [Major] Animation Connections
    + Connected temp animations with player

--- Redhood/player.gd
+++ Redhood/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
+@onready var player_sprite: AnimatedSprite2D = $PlayerSprite
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
-@onready var player_sprite: AnimatedSprite2D = $PlayerSprite
+var speed_modifier = 1.0
+var adjusted_speed = SPEED * speed_modifier
 
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -18,20 +22,35 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("crouch"):
+		speed_modifier = 0.5
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		speed_modifier = 1.0
 	
-	if velocity.x == 0.0:
+	adjusted_speed = SPEED * speed_modifier
+	if direction:
+		velocity.x = direction * adjusted_speed
+		player_sprite.scale.x = sign(velocity.x) * abs(player_sprite.scale.x);
+	else:
+		velocity.x = move_toward(velocity.x, 0, adjusted_speed)
+		
+	move_and_slide()
+
+	if Input.is_action_pressed("crouch") and is_on_floor() and (velocity.x > 0.0 or velocity.x < 0.0):
+		player_sprite.play("crouch_walk")
+	elif Input.is_action_pressed("crouch") and is_on_floor():
+		player_sprite.play("crouch")	
+	elif velocity.x == 0.0:
 		player_sprite.play("idle")
 	elif velocity.x > 0 or velocity.x < 0:
 		player_sprite.play("walk")
-		player_sprite.scale.x = sign(velocity.x) * abs(player_sprite.scale.x);
 	
 	if velocity.y < 0.0 and not is_on_floor():
 		player_sprite.play("jump")
 	elif velocity.y > 0.0 and not is_on_floor():
 		player_sprite.play("fall")
 	
-	move_and_slide()
+	if is_on_wall() and is_on_floor() and direction:
+		player_sprite.play("push_idle")
+	elif is_on_wall() and not is_on_floor() and ray_cast_2d.is_colliding():
+		player_sprite.play("wall_land")
